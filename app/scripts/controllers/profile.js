852b7b1f4e0dbd1c6cafca8c48333ef2f9574f34
U-MILKYWAY\hzhu <hzhu@FZTPFV1.olin.edu>
2014-12-05 22:21:48 -0500

    Basic profile information loaded.

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -8,9 +8,70 @@
  * Controller of the mavenAngularApp
  */
 
-var 
+var samProfile = {
+	"name": "Sam",
+	"video": "gefPe38yo9g",
+	"question": "What is my question?",
+	"experience": ["Cats", "Dogs"],
+	"passions": ["Birds", "Balloons"]
+}
 
+var sarahProfile = {
+	"name": "Sarah",
+	"video": "Mz9ZHGbq0f4",
+	"question": "What is another question?",
+	"experience": ["Cats", "Dogs"],
+	"passions": ["Birds", "Balloons"]
+}
+
+var vickyProfile = {
+	"name": "Vicky",
+	"video": "97LgGWhJphE",
+	"question": "I'm running out of ideas.",
+	"experience": ["Cats", "Dogs"],
+	"passions": ["Birds", "Balloons"]
+}
+
+var vinceProfile = {
+	"name": "Vince",
+	"video": "vOJ9Rcmijh0",
+	"question": "We can fill these in later.",
+	"experience": ["Cats", "Dogs"],
+	"passions": ["Birds", "Balloons"]
+}
+
+var profileList = [samProfile, sarahProfile, vickyProfile, vinceProfile];
 
 angular.module('mavenAngularApp')
-  .controller('ProfileCtrl', function ($scope) {
+  .controller('ProfileCtrl', function ($scope, $sce) {
+  	$scope.profilenum = 0;
+
+  	$scope.userProfile = profileList[$scope.profilenum];
+  	$scope.video = $scope.userProfile.video;
+
+  	$scope.profilePlus = function(){
+  		$scope.profilenum = $scope.profilenum + 1;
+
+  		if ($scope.profilenum >= profileList.length) {
+  			$scope.profilenum = 0;
+  		}
+
+  		$scope.userProfile = profileList[$scope.profilenum];
+  		$scope.video = $scope.userProfile.video;
+  	}
+  })
+  .directive('userVideo', function($sce) {
+  	return {
+  	  restrict: 'EA',
+  	  scope: { video:'=' },
+  	  replace: true,
+  	  template: '<div style="height:400px;"><iframe style="overflow:hidden;height:100%;width:100%" width="100%" height="100%" src="{{url}}" frameborder="0" allowfullscreen></iframe></div>',
+  	  link: function (scope) {
+  	      scope.$watch('video', function (newVal) {
+  	         if (newVal) {
+  	             scope.url = $sce.trustAsResourceUrl("http://www.youtube.com/embed/" + newVal + "?hd=1&rel=0&autohide=1&showinfo=0");
+  	         }
+  	      });
+  	  }
+  	};
   });
