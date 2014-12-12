42932936ed94eb871bba178f25649cf279552ea0
U-MILKYWAY\hzhu <hzhu@FZTPFV1.olin.edu>
2014-12-12 11:48:37 -0500

    Fixed the ask and answer.

---
+++ app/scripts/controllers/intro.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mavenAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mavenAngularApp
+ */
+angular.module('mavenAngularApp')
+  .controller('IntroCtrl', function ($scope) {
+  });
